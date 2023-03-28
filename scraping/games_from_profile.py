e2e2c712c3f19a0702b8bf2b44f0375167aab7cc
Andy <azhang19642@gmail.com>
2023-03-28 18:50:26 -0400

    bs4

--- scraping/games_from_profile.py
+++ scraping/games_from_profile.py
@@ -1,17 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+from requests import session
 
 
-def get_app_ids(profile_id: int | str):
+def get_app_ids(profile_id: int | str = 'DadFox1'):
+    cookies = {'birthtime': '283993201',
+               'mature_content': '1'}
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+
     if isinstance(profile_id, str):
         response = requests.get(url=f'https://steamcommunity.com/profiles/{profile_id}/games/?tab=all',
-                                headers={'User-Agent': 'Mozilla/5.0'})
+                                headers=headers,
+                                cookies=cookies)
     else:
         response = requests.get(url=f'https://steamcommunity.com/id/{profile_id}/games/?tab=all',
-                                headers={'User-Agent': 'Mozilla/5.0'})
+                                headers=headers,
+                                cookies=cookies)
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    app_id = soup.find(class_='search_result_row')['data-ds-appid']
-
-
-return app_id
+    return soup
--- scraping/reviews_from_game.py
+++ scraping/reviews_from_game.py
@@ -2,12 +2,17 @@ import requests
 
 
 def get_reviews(appid, params={'json': 1}):
+    """Return the json of the game review page
+    """
     url = 'https://store.steampowered.com/appreviews/'
     response = requests.get(url=url + appid, params=params, headers={'User-Agent': 'Mozilla/5.0'})
     return response.json()
 
 
 def get_n_reviews(appid='1172380', n=10):
+    """Return a list of reviews of appid where each review is a dictionary containing metadata around the review.
+    There are n reviews, sorted by the most helpful.
+    """
     reviews = []
     cursor = '*'
     params = {
@@ -19,7 +24,7 @@ def get_n_reviews(appid='1172380', n=10):
         'purchase_type': 'all'
     }
 
-    while n > 0:
+    while n > 0:  # Each json response only gets 100 reviews, so this is a way around it if n > 100
         params['cursor'] = cursor.encode()
         params['num_per_page'] = min(100, n)
         n -= 100
