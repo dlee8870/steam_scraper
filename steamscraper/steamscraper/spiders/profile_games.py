c22c9dc3a39faec5d2a39d3f9937c0b26a4ff08b
Andy <azhang19642@gmail.com>
2023-03-28 13:58:08 -0400

    ahmed

--- steamscraper/steamscraper/spiders/profile_games.py
+++ steamscraper/steamscraper/spiders/profile_games.py
@@ -1,25 +1,23 @@
-import scrapy
-
-
-class ReviewSpider(scrapy.Spider):
-    name = 'profile_games'
-
-    def start_requests(self):
-        urls = [
-            'https://steamcommunity.com/app/1172380'
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url, method='GET', callback=self.parse)
-
-    def parse(self, response):
-        # Load all reviews on current page.
-        reviews = response.css('div.apphub_UserReviewCardContent')  # div of each response box
-        for review in reviews:
-            yield {
-                "text": review.xpath(''.join('.//*[@class="apphub_CardTextContent"]//text()')).getall()[-1].replace(
-                    '\r\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '').replace('\t\t\t', ''),
-                "Recommend": review.css('div.title::text').get(),
-                "date": review.css('div.date_posted::text').get(),
-                'found_helpful': review.css('div.review_award_aggregated.tooltip').get()
-            }
+# from scrapy import Spider
+# from scrapy.spiders import Rule
+#
+#
+# class SteamSpider(Spider):
+#     name = 'products'
+#     start_urls = ["http://store.steampowered.com/search/?sort_by=Released_DESC"]
+#     allowed_domains = ["steampowered.com"]
+#     rules = [
+#         Rule(
+#             LinkExtractor(
+#                 allow='/app/(.+)/',
+#                 restrict_css='#search_result_container'),
+#             callback='parse_product'),
+#         Rule(
+#             LinkExtractor(
+#                 allow='page=(d+)',
+#                 restrict_css='.search_pagination_right'))
+#     ]
+#
+#
+# username = "steam_scraping_temp"
+# password = "JANSF958^%195ma"
