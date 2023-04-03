3e2bc6a37103948800f975c15ec7e9afeec00ec3
CO37 <86143600+jchrisoh@users.noreply.github.com>
2023-04-02 23:01:07 -0400

    Update games_network.py

--- games_network.py
+++ games_network.py
@@ -224,7 +224,9 @@ def create_recommendation_network(user_games: dict[Game, int],
 
         for recommended_app_id in recommendations:
             recommended_game = get_game_data(recommended_app_id)
-            network.add_recommendation(game, recommended_game)
+            total_weight = sum(recommendations[app_id] for app_id in recommendations)
+            game_weight = recommendations[recommended_app_id] / total_weight
+            network.add_recommendation(game, recommended_game, game_weight)
             if recommended_game not in visited_games:
                 games_queue.put_nowait(recommended_game)
 
