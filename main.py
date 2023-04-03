6f677d06e0beecb0546f0f51e187cd239a9003ef
CO37 <86143600+jchrisoh@users.noreply.github.com>
2023-04-03 00:08:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ This file is Copyright (c) 2023 Chris Oh, Ahmed Hassini, Andy Zhang, Daniel Lee
 """
 from scrape_app_ids import scrape_app_ids
 from games_network import create_recommendation_network
-from scrape_game_data import get_game_data
 from input_data import run_tkinter
 
 # Note to self: Need to split up program into the following files according to assignment page:
@@ -27,19 +26,9 @@ if __name__ == '__main__':
     run_tkinter()
 
     # 1. User is prompted to enter their profile_id using code in input_data.py
-    profile_id = 76561198976283119
     # 2. Their profile_id is passed into scrape_app_ids() in scrape_app_ids.py which returns a set of app_ids
-    app_ids = list(scrape_app_ids(profile_id, 10))
     # 3. Convert each app_ids into Game objects with corresponding instance attributes.
-    user_games = {}
-
-    for app_id in app_ids:
-        game = get_game_data(app_id)
-        user_games[app_id] = game
-
-    # 3. Pass user_games into create_recommendation_network()
-    games_network = create_recommendation_network(user_games)
-
-    # 4. Decision tree
-    # 5. Output/Results
+    # 4. Pass user_games into create_recommendation_network()
+    # 5. Decision tree
+    # 6. Output/Results
 
